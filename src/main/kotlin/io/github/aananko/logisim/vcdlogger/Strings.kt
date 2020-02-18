22e225d11cf9e6ecbe2c73431cb23e4a1463fb89
aananko <a.g.ananko@gmail.com>
2020-02-18 16:55:26 +0700

    add localization helper singletone

---
+++ src/main/kotlin/io/github/aananko/logisim/vcdlogger/Strings.kt
@@ -0,0 +1,26 @@
+package io.github.aananko.logisim.vcdlogger
+
+import com.cburch.logisim.util.LocaleManager
+import com.cburch.logisim.util.LocaleListener
+import java.util.*
+
+object Strings {
+    private const val resourceBundleName = "testBundle"
+    private val defaultBundle: ResourceBundle = ResourceBundle.getBundle(resourceBundleName, Locale("en"))
+    private var bundle = getBundle()
+    private val localeListener = LocaleListener { bundle = getBundle() }
+
+    init { LocaleManager.addLocaleListener(localeListener) }
+    fun finalize() { LocaleManager.removeLocaleListener(localeListener) }
+
+    private fun getBundle(): ResourceBundle =
+        try { ResourceBundle.getBundle(resourceBundleName) }
+        catch (e: MissingResourceException) { defaultBundle }
+
+    fun get(key: String): String =
+        try { bundle.getString(key) }
+        catch (e: MissingResourceException) {
+            try { defaultBundle.getString(key) }
+            catch (e: MissingResourceException) { key }
+        }
+}
