2f9dcb556adb2e5e51cece3b5c058c2c579a32fc
aananko <a.g.ananko@gmail.com>
2020-02-20 17:14:12 +0700

    move downladed logisim jar to build dir
    to make gradle clean behave correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "io.github.aananko.logisim"
 version = "0.1-SNAPSHOT"
 
 val logisimVersion = "2.7.1"
-val logisimJar = "logisim/logisim-generic-${logisimVersion}.jar"
+val logisimJar = "${buildDir}/logisim/logisim-generic-${logisimVersion}.jar"
 val logisimUrl = "https://sourceforge.net/projects/circuit/files/${
         logisimVersion.replaceAfterLast('.', "x")
     }/${logisimVersion}/logisim-generic-${logisimVersion}.jar/download"
